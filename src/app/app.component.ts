e889d44a21f8fcc243e8c483b17394f98e37fe75
Esau Orozco <esau.orozco.r@gmail.com>
2022-10-06 13:17:04 -0500

    custom form code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  delay,
+  filter,
+  map,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'custom-form';
+export class AppComponent implements OnDestroy {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    code: new FormControl('', Validators.required),
+  });
+
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  get code() {
+    return this.form.get('code');
+  }
+
+  inProgress = false;
+  codeIcon = '';
+
+  private unsubscribe$: Subject<void> = new Subject<void>();
+
+  constructor() {
+    this.onCodeChange().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  private onCodeChange(): Observable<void> {
+    return this.form.get('code')!.valueChanges.pipe(
+      takeUntil(this.unsubscribe$),
+      tap((code) => {
+        this.inProgress = !!code;
+        this.codeIcon = '';
+      }),
+      debounceTime(1000),
+      filter((code) => !!code),
+      switchMap((code) => this.validateCode(code)),
+      map((isValid) => {
+        this.inProgress = false;
+        this.codeIcon = isValid ? 'check_circle' : 'error';
+        if (!isValid) {
+          this.code?.setErrors({ invalidCode: true });
+          this.code?.markAllAsTouched();
+        }
+      })
+    );
+  }
+
+  private validateCode(code: string | null): Observable<boolean> {
+    return new Observable<boolean>((obs) => {
+      if (code === 'code') return obs.next(true);
+      return obs.next(false);
+    }).pipe(delay(500));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
